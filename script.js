1c861b5202404c8a7eca34b4d8bc8ab2c15a6790
Denis <patron.2001den@mail.ru>
2023-12-10 16:25:15 +0700

    Second lesson is ready

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -33,16 +10,26 @@ let personalMovieDB = {
     privat: false
 }
 
-let lastMovie = prompt('Один из последних просмотренных фильмов?', '');
-let lastMovieGrade = +prompt('На сколько оцените его?', '');
-let lastMovie2 = prompt('Один из последних просмотренных фильмов?', '');
-let lastMovieGrade2 = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < personalMovieDB.count; i++) {
+    let lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+    let lastMovieGrade = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastMovie] = lastMovieGrade;
-personalMovieDB.movies[lastMovie2] = lastMovieGrade2;
+    if (lastMovie != null && lastMovie != '' && lastMovieGrade != null && lastMovieGrade != '') {
+        personalMovieDB.movies[lastMovie] = lastMovieGrade;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
-
-let str = "New branch";
-
-let longStr = "dsadas";
